f0453429469dfe46d3e12ec5ab20573f247bc5ba
alexKor <alex8455@example.coman>
2018-05-06 13:50:31 +0400

    webpack_001 Creating a bundle

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-(function() {
-    'use strict';
-    const component = () => {
-        const element = document.createElement('div');
-      
-        // Lodash, currently included via a script, is required for this line to work
-        element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-      
-        return element;
-    }
-      
-    document.body.appendChild(component());
-}());
+import _ from 'lodash';
+
+'use strict';
+const component = () => {
+    const element = document.createElement('div');
+    
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'Alex'], ' ');
+    
+    return element;
+}
+    
+document.body.appendChild(component());
