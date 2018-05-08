31fea6590783063b7126eef6df4df68579879577
Alexey Korablin <Alexey_Korablin@epam.com>
2018-05-08 11:57:40 +0300

    webpack4_002 Asset managenent.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import _ from 'lodash';
 import style from './style.css';
+import icon from './icon.png';
+import data from './data.xml';
 
 'use strict';
 const component = () => {
@@ -8,6 +10,12 @@ const component = () => {
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'Alex'], ' ');
     element.classList.add('hello');
+
+    let myIcon = new Image();
+    myIcon.src = icon;
+    element.appendChild(myIcon);
+
+    console.log(data);
     
     return element;
 }
