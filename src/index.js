1c0e23452121858b6736e4dbc2274e06e56c60ae
Alexey Korablin <Alexey_Korablin@epam.com>
2018-05-10 18:09:19 +0300

    webpack4_005 Development. Watching, dev-server, middleware(express)

--- src/index.js
+++ src/index.js
@@ -2,20 +2,27 @@ import _ from 'lodash';
 import style from './style.css';
 import icon from './icon.png';
 import data from './data.xml';
+import printMe from './print.js'
 
 'use strict';
 const component = () => {
     const element = document.createElement('div');
-    
+    const btn = document.createElement('button');
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'Alex'], ' ');
     element.classList.add('hello');
+
+    btn.innerHTML = 'Click me to get result in the console!';
 
     let myIcon = new Image();
     myIcon.src = icon;
     element.appendChild(myIcon);
 
     console.log(data);
+
+    btn.addEventListener('click', printMe);
+
+    element.appendChild(btn)
     
     return element;
 }
