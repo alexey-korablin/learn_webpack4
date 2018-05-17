a6497f23eebb71a9fceee50364547e6d6bc78b6a
Alexey Korablin <Alexey_Korablin@epam.com>
2018-05-17 12:26:51 +0300

    webpack4_010 Asset management

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import _ from 'lodash';
+import './style.css';
+import icon from './icon.png';
+import data from './data.xml';
 
 function component() {
 var element = document.createElement('div');
 
+const myIcon = new Image();
+myIcon.src = icon;
+
 // Lodash, now imported by this script
 element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+element.classList.add('hello');
+element.appendChild(myIcon);
+
+console.log(data);
 
 return element;
 }
